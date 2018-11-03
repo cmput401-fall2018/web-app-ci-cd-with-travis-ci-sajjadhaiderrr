d7c6c42989773f18ffbc23e496d4c89d1a5a76e8
Sajjad Haider <shaider1@ualberta.ca>
2018-11-02 22:40:17 -0600

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
 def test_home():
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome('/usr/bin/chromedriver')
     driver.get("http://204.209.76.213:8000/")
 
     assert driver.find_element_by_id("name") == "Sajjad Haider"
