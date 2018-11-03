df12fc58ee2004ddb92bd70cfac87639cc45b078
Sajjad Haider <shaider1@ualberta.ca>
2018-11-02 22:41:43 -0600

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+import os
 
 def test_home():
-    driver = webdriver.Chrome('/usr/bin/chromedriver')
+    option.add_argument("headless")
+    driverPath = os.getcwd() + '/chromedriver'
+    driver = webdriver.Chrome(driverPath, options=option)
     driver.get("http://204.209.76.213:8000/")
 
     assert driver.find_element_by_id("name") == "Sajjad Haider"
