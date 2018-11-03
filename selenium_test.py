9bfa75686a1ddf4fa8d9c6cc6df9e2e957b9d188
Sajjad Haider <shaider1@ualberta.ca>
2018-11-02 19:33:06 -0600

    Create selenium_test.py

---
+++ selenium_test.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+def test_home():
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.get("http://204.209.76.213:8000/")
+
+    assert driver.find_element_by_id("name") == "Sajjad Haider"
+    
+    assert driver.find_element_by_id("about") == "Aspiring Software Developer"
+    
+    assert driver.find_element_by_id("education") == "Bachelor of Science in Computing Science and Psychology"
+    
+    assert driver.find_element_by_id("skills") == "React,Redux,Java,Python,C"
+    
+    assert driver.find_element_by_id("work") == "Tangent Collective 6 months"
+    
+    assert driver.find_element_by_id("contact") == "780-920-8128"  
+
+test_home()
+
